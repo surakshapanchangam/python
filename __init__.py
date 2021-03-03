e66e202e34527f90290c2f727ce97f8864ac46c3
surakshapanchangam <53073928+surakshapanchangam@users.noreply.github.com>
2021-03-03 15:52:38 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,32 @@
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+# User input
+
+
+choice =(input("Enter operator to use:"))
+A = int(input("Enter first number: "))
+B = int(input("Enter second number: "))
+if choice == '+':
+    print(A, "+", B, "=", add(A, B))
+elif choice == '-':
+    print(A, "-", B, "=", subtract(A, B))
+elif choice == '*':
+    print(A, "*", B, "=", multiply(A, B))
+elif choice == '/':
+    print(A, "/", B, "=", divide(A, B))
+else:
+    print("Invalid input")
