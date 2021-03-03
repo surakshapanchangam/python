7d7195b4129a1cca8985f5e258cc8b14a053c6f5
surakshapanchangam <53073928+surakshapanchangam@users.noreply.github.com>
2021-03-03 18:00:43 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,32 +1,43 @@
+class cal():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
-def add(a, b):
-    return a + b
+    def add(self):
+        return self.a + self.b
+
+    def mul(self):
+        return self.a * self.b
+
+    def div(self):
+        return self.a / self.b
+
+    def sub(self):
+        return self.a - self.b
 
 
-def subtract(a, b):
-    return a - b
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+obj = cal(a, b)
+choice = 1
+while choice != 0:
+    print("0. Exit")
+    print("1. Add")
+    print("2. Subs")
+    print("3. Mul")
+    print("4. Div")
+    choice = int(input("Entr choice: "))
+    if choice == 1:
+        print("Rslt: ", obj.add())
+    elif choice == 2:
+        print("Rslt: ", obj.sub())
+    elif choice == 3:
+        print("Rslt: ", obj.mul())
+    elif choice == 4:
+        print("Rslt: ", round(obj.div(), 2))
+    elif choice == 0:
+        print("no operation")
+    else:
+        print("Invalid choice")
 
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    return a / b
-
-# User input
-
-
-choice =(input("Enter operator to use:"))
-A = int(input("Enter first number: "))
-B = int(input("Enter second number: "))
-if choice == '+':
-    print(A, "+", B, "=", add(A, B))
-elif choice == '-':
-    print(A, "-", B, "=", subtract(A, B))
-elif choice == '*':
-    print(A, "*", B, "=", multiply(A, B))
-elif choice == '/':
-    print(A, "/", B, "=", divide(A, B))
-else:
-    print("Invalid input")
+#print()
